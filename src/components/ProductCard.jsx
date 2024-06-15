2b7c2bda1e0139d700fe79fbd15f2e42f4469732
Vedant005 <vedantkanekar909@gmail.com>
2024-06-15 23:47:55 +0530

    added prouct card structure

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function ProductCard() {
+  return (
+    <div className='main'>
+      <div className='Image-div & wishlist icon'>
+
+      </div>
+      <div className='Product Details'>
+
+      </div>
+      <div className='cart-button'>
+
+      </div>
+    </div>
+  )
+}
+
+export default ProductCard
