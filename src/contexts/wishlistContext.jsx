881ea0e752958f83d7d9f6ff9fcc333d909e700e
Vedant005 <vedantkanekar909@gmail.com>
2024-06-22 00:31:07 +0530

    wishlist context  added

---
+++ src/contexts/wishlistContext.jsx
@@ -0,0 +1,28 @@
+
+
+
+import { createContext, useState } from "react";
+
+
+export const WishlistContext = createContext();
+ export const WishlistProvider = ({ children }) => {
+
+    const [wishList, setWishList] = useState([]);
+
+    const addToWishListHandler = (product) => {
+        wishList.find((item) => item._id === product._id) ? setWishList([...wishList].filter((item) => item._id !== product._id)) : setWishList([...wishList, product]);
+
+
+    }
+
+
+
+    return (<WishlistContext.Provider value={{ wishList,setWishList, addToWishListHandler }}>
+        {children}
+    </WishlistContext.Provider>)
+
+
+}
+
+
+
