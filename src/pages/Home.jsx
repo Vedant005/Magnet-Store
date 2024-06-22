b07f97bdd1b9ac3792128c3bc0c881ce056af661
Vedant005 <vedantkanekar909@gmail.com>
2024-06-22 23:36:08 +0530

    prettieer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,60 +1,77 @@
-import React from 'react';
-import Header  from '../components/Header';
+import React from "react";
+import Header from "../components/Header";
 
 export default function App() {
   return (
     <div className="bg-gray-400">
-      <div className='sticky top-0 z-10'>
+      <div className="sticky top-0 z-10">
         <Header />
       </div>
       <div
         style={{
           backgroundImage: `url(https://images.unsplash.com/photo-1607384070812-0965d8827f6f?q=80&w=2021&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)`,
-          
         }}
-        className='w-full h-[600px] bg-cover bg-center flex items-center justify-center'
+        className="w-full h-[600px] bg-cover bg-center flex items-center justify-center"
       >
         <div className="text-center bg-black bg-opacity-50 p-6 rounded-lg">
-          <h1 className="text-4xl font-bold mb-4 text-white">Unveiling the New</h1>
-          <p className="text-lg mb-4 text-white">UPTO 80% OFF on various products</p>
-          <button className="bg-green-500 text-white py-2 px-4 rounded-lg">Shop Now</button>
+          <h1 className="text-4xl font-bold mb-4 text-white">
+            Unveiling the New
+          </h1>
+          <p className="text-lg mb-4 text-white">
+            UPTO 80% OFF on various products
+          </p>
+          <button className="bg-green-500 text-white py-2 px-4 rounded-lg">
+            Shop Now
+          </button>
         </div>
       </div>
-      <div className='w-full h-10'></div>
-      <div className='w-full h-10'></div>
-      
-      
-      <div className='text-center'>
-        <h1 className='font-bold underline '>FEATURED PRODUCTS</h1>
-      <div className='flex justify-center'>
-        
-        <div className='grid gap-4 sm:grid-cols-2 md:grid-cols-2  mx-2 my-5'>
-          <div style={{backgroundImage: `url(https://images.unsplash.com/photo-1567763745030-bfe9c51bec27?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)`,
-              backgroundSize: 'cover',
-              backgroundPosition: 'center'
-          }} className='min-h-[300px] w-[400px] rounded bg-orange-400 shadow-xl flex items-center justify-center' >
-             
-          </div>
-          <div style={{backgroundImage: `url(https://images.unsplash.com/photo-1567763745030-bfe9c51bec27?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)`,
-              backgroundSize: 'cover',
-              backgroundPosition: 'center'
-          }} className='min-h-[300px] w-[100%] rounded bg-orange-400 shadow-xl flex items-center justify-center'>
-            <p>Card 2</p>
-          </div>
-          <div style={{backgroundImage: `url(https://images.unsplash.com/photo-1567763745030-bfe9c51bec27?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)`,
-              backgroundSize: 'cover',
-              backgroundPosition: 'center'
-          }} className='min-h-[300px] w-[100%] rounded bg-orange-400 shadow-xl flex items-center justify-center'>
-            <p>Card 3</p>
-          </div>
-          <div style={{backgroundImage: `url(https://images.unsplash.com/photo-1567763745030-bfe9c51bec27?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)`,
-              backgroundSize: 'cover',
-              backgroundPosition: 'center'
-          }} className='min-h-[300px] w-[100%] rounded bg-orange-400 shadow-xl flex items-center justify-center'>
-            <p>Card 4</p>
+      <div className="w-full h-10"></div>
+      <div className="w-full h-10"></div>
+
+      <div className="text-center">
+        <h1 className="font-bold underline ">FEATURED PRODUCTS</h1>
+        <div className="flex justify-center">
+          <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-2  mx-2 my-5">
+            <div
+              style={{
+                backgroundImage: `url(https://images.unsplash.com/photo-1567763745030-bfe9c51bec27?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+              }}
+              className="min-h-[300px] w-[400px] rounded bg-orange-400 shadow-xl flex items-center justify-center"
+            ></div>
+            <div
+              style={{
+                backgroundImage: `url(https://images.unsplash.com/photo-1567763745030-bfe9c51bec27?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+              }}
+              className="min-h-[300px] w-[100%] rounded bg-orange-400 shadow-xl flex items-center justify-center"
+            >
+              <p>Card 2</p>
+            </div>
+            <div
+              style={{
+                backgroundImage: `url(https://images.unsplash.com/photo-1567763745030-bfe9c51bec27?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+              }}
+              className="min-h-[300px] w-[100%] rounded bg-orange-400 shadow-xl flex items-center justify-center"
+            >
+              <p>Card 3</p>
+            </div>
+            <div
+              style={{
+                backgroundImage: `url(https://images.unsplash.com/photo-1567763745030-bfe9c51bec27?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+              }}
+              className="min-h-[300px] w-[100%] rounded bg-orange-400 shadow-xl flex items-center justify-center"
+            >
+              <p>Card 4</p>
+            </div>
           </div>
         </div>
-      </div>
       </div>
     </div>
   );
--- src/pages/Wishlist.jsx
+++ src/pages/Wishlist.jsx
@@ -1,69 +1,54 @@
 import Header from "../components/Header";
 import { WishlistContext } from "../contexts/wishlistContext";
 
-
 import { useContext } from "react";
 
+export default function Wishlist() {
+  const { wishList } = useContext(WishlistContext);
 
-
-export default function Wishlist(){
-
-    const {wishList}= useContext(WishlistContext);
-
-    return(
-        <div>
-           <div>
-            <Header/>
-           </div>
-           <h1> Items in wishList: {wishList.length} </h1>
-        <div className="wishlist">
+  return (
+    <div>
+      <div>
+        <Header />
+      </div>
+      <h1> Items in wishList: {wishList.length} </h1>
+      <div className="wishlist">
         <div className="wishList-info-card">
-     
-           {wishList.length ===0 ?(
-              <div>
-                 <p>There is nothing in wishList</p>
-                 </div>
-           ):(
-           <div className="wishList-main">
-               {
-                 wishList.map((item)=>{
-                    const {_id,title,price,ratings}=item;
-                    return(
-                    <div className="wishList-product-main" key={_id}>
-                       
-                          
-     
-                          <div className="wishList-details-div">
-                                <h1>{title}</h1>
-                               
-                               <p>Rating ={ratings}</p>
-     
-                                   <p>Price ={price}</p>
-                                        <div className="quantity">
-                                    {/* <button className="decrement"  >-</button>
+          {wishList.length === 0 ? (
+            <div>
+              <p>There is nothing in wishList</p>
+            </div>
+          ) : (
+            <div className="wishList-main">
+              {wishList.map((item) => {
+                const { _id, title, price, ratings } = item;
+                return (
+                  <div className="wishList-product-main" key={_id}>
+                    <div className="wishList-details-div">
+                      <h1>{title}</h1>
+
+                      <p>Rating ={ratings}</p>
+
+                      <p>Price ={price}</p>
+                      <div className="quantity">
+                        {/* <button className="decrement"  >-</button>
                                    <div type="Number" className="quantityDiv">9</div>
                                  <button className="increment" >+</button> */}
-                                   
-                             </div>
-                             <div>
-                             <button className="wishListItemActionBtn" > Remove</button>
-                                </div>
-                                
-                         </div>
-                          </div>
-                          
-                    )
-                                  
-                 }
-                 )
-               }
-              </div>
-              )}
-      
+                      </div>
+                      <div>
+                        <button className="wishListItemActionBtn">
+                          {" "}
+                          Remove
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          )}
         </div>
-        </div>
-        
-        </div>
-       )
-
+      </div>
+    </div>
+  );
 }
