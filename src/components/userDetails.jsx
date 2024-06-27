0c21c37d1d1d309a7ec7945acad581cbf11adeec
Vedant005 <vedantkanekar909@gmail.com>
2024-06-27 23:54:28 +0530

    user deatils page

---
+++ src/components/userDetails.jsx
@@ -0,0 +1,20 @@
+import { useUser } from "../contexts/UserContext";
+import { Header } from "./Header";
+
+export const UserDetails = () => {
+  const { authState, userLogout } = useUser();
+
+  return (
+    <div className="user-class">
+      <Header />
+      <h1>
+        {authState?.user?.firstName} {authState?.user?.lastName}
+      </h1>
+      <p>{authState?.user?.email}</p>
+
+      <button onClick={userLogout} className="logout-btn">
+        Logout
+      </button>
+    </div>
+  );
+};
