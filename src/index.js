ae1e0c8d20fdcc539c15d0b902346277e8819f42
Vedant005 <vedantkanekar909@gmail.com>
2024-12-01 23:22:46 +0530

    import changes

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { ProductProvider } from "./contexts/productContext.jsx";
 import FilterProvider from "./contexts/filterContext.jsx";
 import { CartProvider } from "./contexts/cartContext.jsx";
 import { WishlistProvider } from "./contexts/wishlistContext.jsx";
-import AuthProvider from "./contexts/authContext.jsx";
+import { AuthProvider } from "./contexts/authContext.jsx";
 
 makeServer();
 ReactDOM.createRoot(document.getElementById("root")).render(
