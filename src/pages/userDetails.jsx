9384ac95de9b317e7dd9bc9fd355bb9af2ad7d0d
Vedant005 <vedantkanekar909@gmail.com>
2024-12-02 13:28:46 +0530

    handled navigation

--- src/pages/userDetails.jsx
+++ src/pages/userDetails.jsx
@@ -1,9 +1,16 @@
-import React, { useContext } from "react";
+import React from "react";
 import Header from "../components/Header";
-import { AuthContext } from "../contexts/authContext";
+import { useAuth } from "../contexts/authContext";
+import { useNavigate } from "react-router-dom";
 
 export const UserDetails = () => {
-  const { authState, userLogout } = useContext(AuthContext);
+  const { currentUser, logout } = useAuth();
+
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    logout();
+    navigate("/login");
+  };
 
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col">
@@ -11,12 +18,12 @@ export const UserDetails = () => {
       <div className="flex-grow flex items-center justify-center">
         <div className="bg-white rounded-lg shadow-md p-8 w-full max-w-lg">
           <h1 className="text-2xl font-bold mb-4">
-            {authState?.user?.firstName} {authState?.user?.lastName}
+            {currentUser?.firstName} {currentUser?.lastName}
           </h1>
-          <p className="text-gray-700 mb-6">{authState?.user?.email}</p>
+          <p className="text-gray-700 mb-6">{currentUser?.email}</p>
 
           <button
-            onClick={userLogout}
+            onClick={handleLogout}
             className="bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-600 transition duration-300"
           >
             Logout
