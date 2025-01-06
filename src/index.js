52e22631aeef69c9fe2544fc401eea4917601d36
Vedant005 <vedantkanekar909@gmail.com>
2025-01-06 19:40:08 +0530

    refactor: simplify index.js by removing unnecessary context providers and adding ToastContainer

--- src/index.js
+++ src/index.js
@@ -2,29 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { makeServer } from "./server.js";
+import { ToastContainer } from "react-toastify";
 import { BrowserRouter as Router } from "react-router-dom";
-import { ProductProvider } from "./contexts/productContext.jsx";
-import FilterProvider from "./contexts/filterContext.jsx";
-import { CartProvider } from "./contexts/cartContext.jsx";
-import { WishlistProvider } from "./contexts/wishlistContext.jsx";
-import { AuthProvider } from "./contexts/authContext.jsx";
 
-makeServer();
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
-      <AuthProvider>
-        <ProductProvider>
-          <FilterProvider>
-            <CartProvider>
-              <WishlistProvider>
-                <App />
-              </WishlistProvider>
-            </CartProvider>
-          </FilterProvider>
-        </ProductProvider>
-      </AuthProvider>
+      <App />
     </Router>
+    <ToastContainer />
   </React.StrictMode>
 );
