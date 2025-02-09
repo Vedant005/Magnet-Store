284d4429cbb7e6e2caa0282f026a0e20f3dd4290
Vedant005 <vedantkanekar909@gmail.com>
2025-02-09 23:54:32 +0530

    refactor: streamline loading state management in product store

--- src/stores/productStore.js
+++ src/stores/productStore.js
@@ -2,55 +2,74 @@ import { create } from "zustand";
 import apiClient from "../utils/axiosInterceptor";
 import useFilterStore from "./filterStore";
 
-const useProductStore = create((set) => ({
+const useProductStore = create((set, get) => ({
   products: [],
   singleProduct: {},
-  loading: false,
+  loading: true,
   error: null,
 
   fetchFilteredProducts: async () => {
     const { filters } = useFilterStore.getState();
-
-    set({ loading: true, error: null });
+    set((state) => ({
+      loading: true,
+      error: null,
+    }));
 
     try {
       const response = await apiClient.get("/filter/apply", {
         params: filters,
       });
-      set({ products: response.data.data, loading: false });
-    } catch (error) {
-      set({
-        error: error.response?.data?.message || error.message,
+      set((state) => ({
+        products: response.data.data,
         loading: false,
-      });
+      }));
+    } catch (error) {
+      set((state) => ({
+        error: error.response?.data?.message || error.message,
+        // loading: false,
+      }));
     }
   },
 
   fetchAllProducts: async () => {
-    set({ loading: true, error: null });
+    set((state) => ({
+      loading: true,
+      error: null,
+    }));
 
     try {
       const response = await apiClient.get("/products/get");
-      set({ products: response.data.data, loading: false });
-    } catch (error) {
-      set({
-        error: error.response?.data?.message || error.message,
+      set((state) => ({
+        products: response.data.data,
         loading: false,
-      });
+      }));
+    } catch (error) {
+      console.log(error);
+
+      set((state) => ({
+        error: error.response?.data?.message || error.message,
+        // loading: false,
+      }));
     }
   },
 
   fetchSingleProduct: async (productId) => {
-    set({ loading: true, error: null });
+    set((state) => ({
+      loading: true,
+      error: null,
+    }));
 
     try {
       const response = await apiClient.get(`/products/${productId}`);
-      set({ singleProduct: response.data.data, loading: false });
-    } catch (error) {
-      set({
-        error: error.response?.data?.message || error.message,
+      set((state) => ({
+        singleProduct: response.data.data,
         loading: false,
-      });
+      }));
+    } catch (error) {
+      set((state) => ({
+        error: error.response?.data?.message || error.message,
+        // loading: false,
+      }));
     }
   },
 }));
